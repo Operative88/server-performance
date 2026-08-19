ad9b5c95cee129bd20710c946a59d608d4127045
Operative88 <wojciechkocemba21@gmail.com>
2026-08-19 12:30:28 +0200

    add cpu usage

--- servers-stats.sh
+++ servers-stats.sh
@@ -18,3 +18,13 @@ local non_idle=$((user + nice + system + irq + softirq + steal))
 local total = $((idle_all + non_idle))
 echo "$total $idle_all"
 }
+
+read total1 idle1 <<< "$(read_cpu)"
+sleep 1
+read total2 idle2 <<< "$(read_cpu)"
+
+total_diff=$((total2 - total1))
+idle_diff=((idle2 - idle1))
+cpu_usage=$(awk "BEGIN {printf \"%.1f\", (100*($total_diff-$idle_diff))/$total_diff}")
+
+
